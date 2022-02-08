3079e04778ae1b6636494cf12e9073eaa9295c6c
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2022-02-08 23:46:26 +0530

    Leader

--- Question_Set_2/LEADERS.java
+++ Question_Set_2/LEADERS.java
@@ -7,6 +7,7 @@ public class Main
   public static void main (String[]args)
   {
     Scanner in = new Scanner (System.in);
+	  
       System.out.println ("Enter length of an array: ");
     int len = in.nextInt ();
     int[] arr = new int[len];
